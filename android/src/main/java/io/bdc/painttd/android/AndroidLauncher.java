c6db778a276c40761ab460e7de3ba4ebc26a4717
BlackDeluxeCat <1030767312@qq.com>
2026-03-24 14:24:24 +0800

    reboot1

---
+++ android/src/main/java/io/bdc/painttd/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package io.bdc.painttd.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.bdc.painttd.PaintTD;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new PaintTD(), configuration);
+    }
+}

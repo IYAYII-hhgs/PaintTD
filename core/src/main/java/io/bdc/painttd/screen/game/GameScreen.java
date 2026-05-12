62ae99e999997f51636648820b59b51221c664bc
BlackDeluxeCat <1030767312@qq.com>
2026-05-12 14:25:13 +0800

    refine(core, desktop): debug environment

--- core/src/main/java/io/bdc/painttd/screen/game/GameScreen.java
+++ core/src/main/java/io/bdc/painttd/screen/game/GameScreen.java
@@ -24,7 +24,7 @@ public class GameScreen implements Screen {
     public void show() {
         app.worldView.reset();
         currentWorld = new WorldRuntime(app, app.worldView);
-        WorldConfiguration assembler = new WorldConfiguration().setupMap(200, 160);
+        WorldConfiguration assembler = new WorldConfiguration().setupMap(80, 60);
         assembler.assemble(currentWorld);
 
         placement = new PlacementControl(currentWorld);
